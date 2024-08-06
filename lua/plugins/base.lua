cabf5fa0426f0e4cd36ef051b50fb29c4d4340fe
z1ndabad <taimur.samee@gmail.com>
2024-08-06 00:14:37 -0400

    lazy-loading for autopairs

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -5,8 +5,9 @@ return {
 	},
 	{
 		"windwp/nvim-autopairs",
+		event = "InsertEnter",
 		config = true,
-		-- additional options
+		-- fastwrap options
 		opts = {
 			fast_wrap = {
 				map = "<M-e>",
