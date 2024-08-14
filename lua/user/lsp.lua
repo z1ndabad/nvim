869f041663f383065430195b598a0f8eef05db17
z1ndabad <taimur.samee@gmail.com>
2024-08-14 07:20:52 -0400

    formatting hotkey kind-of fixed

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -12,7 +12,11 @@ local lsp_attach = function(client, bufnr)
 	vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
 	vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
 	vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-	vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+
+	-- use conform.nvim for formatting instead of LSP
+	vim.keymap.set({ "n", "x" }, "<F3>", function()
+		require("conform").format({ async = true })
+	end, opts)
 	vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 end
 
@@ -30,3 +34,5 @@ require("mason-lspconfig").setup({
 		end,
 	},
 })
+
+require("mason-conform").setup()
