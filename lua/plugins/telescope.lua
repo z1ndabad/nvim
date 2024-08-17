5cd3248609035fa065f0f278c68f6beee7c294a4
z1ndabad <taimur.samee@gmail.com>
2024-08-17 03:06:59 -0400

    removed lazy-loading for telescope, whoops

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
     keys = {
         { "<leader>F", "<cmd>Telescope find_files<cr>", desc = "Find files" },
         { "<leader>f", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
